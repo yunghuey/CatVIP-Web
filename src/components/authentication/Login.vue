2b22a8a74c43a1a442f120b80502648ec3aa96a2
YUNG HUEY TONG <yunghuey2002@gmail.com>
2023-11-16 16:58:20 +0800

    add css for login and signup

--- src/components/authentication/Login.vue
+++ src/components/authentication/Login.vue
@@ -1,67 +1,114 @@
 <template>
-    <img class="logo" src="../../assets/logo.png" alt="">
-    <h1>CatViP</h1>
-    <div class="login">
-        <input type="text" v-model="username" placeholder="Enter username"/>
-        <input type="password" v-model="password" placeholder="Enter password"/>
-        <button v-on:click="loginFunc">Sign In</button>
-    </div>
-    <p>
-        <router-link to="/sign-up">Dont't have an account yet? Register here </router-link>
-    </p>
+    <div class="container mt-2 pt-2" >
+        <div class="row"><img class="logo mx-auto" src="../../assets/logo.png" alt=""></div>
+        
+        <div class="row text-center"><h1>CatViP</h1></div>
+
+        <div class="row">
+            <div class="col-12 col-sm-8 col-md-5 mx-auto">
+                <div class="card border-0 shadow bg-light">
+                    <div class="card-body">
+                        <div class="row mx-auto"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+                        </svg></div>
+
+                        <input class="form-control my-2 border-3" type="text" v-model="username" placeholder="Enter username"/>
+                        <input class="form-control my-2 border-3" type="password" v-model="password" placeholder="Enter password"/>
+                        <div class="text-end"><router-link to="/sign-up" class="hyperlink">Forgot password?</router-link> </div>
+
+                        <div class="text-center"><button class="btn" v-on:click="loginFunc">Sign In</button></div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        <div class="row">
+            <div class="col-12 col-sm-8 col-md-6 m-auto text-center mt-2">
+                <p><router-link to="/sign-up" class="hyperlink">Dont't have an account yet? Register here </router-link></p>
+            </div>
+        </div>
+        
+    </div>    
 </template>
 
 <script>
-import { ApiConstant } from "../../repository/APIConstant.js"
-import axios from 'axios';
-export default
-{
-    // name can put same as filename -- name of component
-    name:'Login',
-    data()
+    import { ApiConstant } from "../../repository/APIConstant.js"
+    import axios from 'axios';
+    export default
     {
-        return {
-            username: '',
-            password: '',
-        }
-    },
-    methods: {
-        async loginFunc() {
-        console.log(this.username, this.password);
-        try {
-            const result = await axios.post(ApiConstant.LoginURL, {
-                username: this.username,
-                password: this.password,
-                isMobileLogin: false
-                },
-                {headers: {
-                    "Content-Type": "application/json",
-                    "Access-Control-Allow-Origin": "*",
-                    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-                    'Access-Control-Allow-Headers':  'Origin,Content-Type,X-Requested-With,Accept,Authorization'
-                }}
-            );
-
-            if (result.status === 200) {
-            console.log('sign in done');
-            localStorage.setItem("token", JSON.stringify(result.data));
-            this.$router.push({ name: 'Home' });
-            } else {
-            console.error('Unexpected response:', result);
-            alert('Invalid Login');
+        // name can put same as filename -- name of component
+        name:'Login',
+        data()
+        {
+            return {
+                username: '',
+                password: '',
+      
             }
-        } catch (error) {
-            console.error('Error in login', error);
-            alert('An error occurred during login. Please try again.');
-        }
+        },
+        methods: {
+            async loginFunc() {
+            console.log(this.username, this.password);
+            try {
+                const result = await axios.post(ApiConstant.LoginURL, {
+                    username: this.username,
+                    password: this.password,
+                    isMobileLogin: false
+                });
+
+                if (result.status === 200) {
+                console.log('sign in done');
+                localStorage.setItem("token", JSON.stringify(result.data));
+                this.$router.push({ name: 'Home' });
+                } else {
+                console.error('Unexpected response:', result);
+                alert('Invalid Login');
+                }
+            } catch (error) {
+                console.error('Error in login', error);
+                alert('An error occurred during login. Please try again.');
+            }
+            },
+            
+            // login username: admin, password: abc12345, isMobileLogin: false
+        },
+        mounted(){
+            let user = localStorage.getItem('token');
+            if (user){
+                this.$router.push({name:'Home'});
+            }
+        },
+        
     }
-        // login username: admin, password: abc12345, isMobileLogin: false
-    }
-}
 </script>
 
-<style>
+<style scoped>
+svg{
+    color:var(--gold-neutral-color);
+}
 img.logo{
-    width: 200px;
+    width: 150px;
+}
+button{
+    color: #3c1e08;
+    background-color: #ecd9c9;
+}
+button:hover{
+    border: 1px solid #3c1e08;
+    color: #fff;
+    background-color: #bd9957;
+
+}
+.hyperlink{
+    text-decoration: none;
+    color: #3c1e08;
+}
+.hyperlink:hover{
+    text-decoration: underline;
+}
+input[type="text"]:focus,
+input[type="password"]:focus{
+    border: none;
+    box-shadow: 0 0 0 0.2rem rgba(189,153,87, 0.8);
 }
 </style>
--- src/components/authentication/SignUp.vue
+++ src/components/authentication/SignUp.vue
@@ -1,43 +1,68 @@
 <template>
-    <h1>Sign Up</h1>
-    <img class="logo" src="../../assets/logo.png" alt="">
-    <h3>Create an account as seller</h3>
-    <div class="register">
-        <!-- not completed yet, UI purpose first -->
-        <div class="form-control">
-            <!-- <label for="username">Username</label> -->
-            <input type="text" id="username" placeholder="Enter username">
-        </div>
-        <div class="form-control">
-            <input type="text" placeholder="Enter full name">
 
+    <div class="container mt-2 pt-2" style="width: 80%;">
+        <div class="row mx-auto">
+            <div class="col-2"><img class="logo mx-auto" src="../../assets/logo.png" alt=""></div>
+            <div class="col-5 my-auto"><h3>Create an account as seller</h3></div>
         </div>
-        <div class="form-control">
-            <input type="text" placeholder="Enter email address">
+        <div class="row my-2">
+            <div class="col-5 col-sm-4">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" class="form-control"  id="username" placeholder="Enter username">
+            </div>
+            <div class="col-5 col-sm-4">
+                <label for="fullname" class="form-label">Full name</label>
+                <input type="text" class="form-control" id="fullname"  placeholder="Enter full name">
+            </div>
+        </div>
+        <div class="row my-2">
+            <div class="col-5 col-sm-4">
+                <label for="emailadd" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="emailadd" placeholder="Enter email address">
+            </div>
+            <div class="col-5 col-sm-4">
+                <label for="birthday" class="form-label">Date of Birth:</label>
+                <input type="date" class="form-control" id="birthday" name="birthday" :max="maxBirthday">
+            </div>
+        </div>
 
+        <div class="row my-2">
+            <div class="col-5 col-sm-4">
+                <label for="password">Password</label>
+                <input type="password" id="password" class="form-control" placeholder="Enter password">
+            </div>
+            <div class="col-5 col-sm-4">
+                <label for="confirmpassword">Confirm password</label>
+                <input type="password" id="confirmpassword" class="form-control" placeholder="Confirm password">
+            </div>
         </div>
-        <div class="form-control">
-            <input type="password" placeholder="Enter password">
-
+        <div class="row my-2">
+            <div class="col-2">
+                <label for="gender" class="form-label">Gender</label>
+            </div>
+            <div class="form-check col-3">
+                <input class="form-check-input" type="radio" name="gender" id="male-gender">
+                <label class="form-check-label" for="male-gender">Male</label>
+            </div>
+            <div class="form-check col-3">
+                <input class="form-check-input" type="radio" name="gender" id="female-gender">
+                <label class="form-check-label" for="female-gender">Female</label>
+            </div>
         </div>
-        <div class="form-control">
-            <input type="password" placeholder="Confirm password">
-
+        <div class="row my-2">
+            <div class="col-3">
+                <button class="form-control">Sign Up</button>
+            </div>
         </div>
-        <div class="form-control">
-            <input type="radio" id="male-gender" name="gender" value="male">
-            <label for="male-gender">Male</label>
-            <input type="radio" id="female-gender" name="gender" value="female" v-model="selectedDate">
-        <label for="male-gender">Female</label>
+        <div class="row">
+            <div class="col-4">
+                <router-link to="/login" class="hyperlink">Already have an account? Sign in here </router-link>
+            </div>
         </div>
-        <div class="form-control">
-            <label for="birthday">Birthday:</label>
-            <input type="date" id="birthday" name="birthday" :max="maxBirthday">
-        </div>  
-       
-        <button class="form-control">Sign Up</button>
-        <router-link to="/login">Already have an account? Sign in here </router-link>
-   </div>
+     
+     
+        
+</div>
 </template>
 
 <script>
@@ -67,28 +92,31 @@ export default {
 
 <style>
 img.logo{
-    width: 200px;
+    width: 120px;
 }
-
-.form-control {
-    width: 200px;
-    height: 40px;
-    padding: 10px 20px;
-    margin-left: auto;
-    margin-right: auto;
-    display: flex;
+.hyperlink{
+    text-decoration: none;
+    color: #3c1e08;
 }
-.form-control input{
-    border: 1px solid skyblue;
-
+.hyperlink:hover{
+    text-decoration: underline;
 }
-.register button{
-    width: 320px;
-    height: 40px;
-    border: 2px solid skyblue;
-    border-radius: 20px;
-    background: skyblue;
-    color: black;
-    text-align: center;
+input[type="text"]:focus,
+input[type="password"]:focus,
+input[type="date"]:focus{
+    border: none;
+    box-shadow: 0 0 0 0.2rem rgba(189,153,87, 0.8);
+}
+.row{
+    justify-content: center;
+}
+button{
+    color: #3c1e08;
+    background-color: #ecd9c9;
+}
+button:hover{
+    border: 2px solid #3c1e08;
+    color: #fff;
+    background-color: #bd9957;
 }
 </style>
