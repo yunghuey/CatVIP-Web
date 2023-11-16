a52dbc477097a0af3f5cade36e2c024c99ae003d
YUNG HUEY TONG <yunghuey2002@gmail.com>
2023-11-16 20:04:54 +0800

    add invalid login display

--- src/components/authentication/Login.vue
+++ src/components/authentication/Login.vue
@@ -5,18 +5,21 @@
         <div class="row text-center"><h1>CatViP</h1></div>
 
         <div class="row">
-            <div class="col-12 col-sm-8 col-md-5 mx-auto">
+            <div class="col-12 col-sm-8 col-md-6 mx-auto">
                 <div class="card border-0 shadow bg-light">
                     <div class="card-body">
                         <div class="row mx-auto"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
                         <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
                         </svg></div>
 
-                        <input class="form-control my-2 border-3" type="text" v-model="username" placeholder="Enter username"/>
-                        <input class="form-control my-2 border-3" type="password" v-model="password" placeholder="Enter password"/>
+                        <input :class="{'error-sign': isUsernameError}" class="form-control my-2 border-3" type="text" v-model="username" placeholder="Enter username"/>
+                        <span :class="{'text-danger': isUsernameError}">{{ usernameErrorMessage}}</span>
+                        <input :class="{'error-sign': isPwdError}" class="form-control my-2 border-3" type="password" v-model="password" placeholder="Enter password"/>
+                        <span :class="{'text-danger': isPwdError}">{{ pwdErrorMessage}}</span>
+
                         <div class="text-end"><router-link to="/sign-up" class="hyperlink">Forgot password?</router-link> </div>
 
-                        <div class="text-center"><button class="btn" v-on:click="loginFunc">Sign In</button></div>
+                        <div class="text-center"><input type="submit" value="Sign In" class="btn" v-on:click="loginFunc"/></div>
                     </div>
                 </div>
             </div>
@@ -32,7 +35,8 @@
 </template>
 
 <script>
-    import { ApiConstant } from "../../repository/APIConstant.js"
+    import { EMPTY_ARR } from "@vue/shared";
+import { ApiConstant } from "../../repository/APIConstant.js"
     import axios from 'axios';
     export default
     {
@@ -43,30 +47,49 @@
             return {
                 username: '',
                 password: '',
+                isUsernameError: false,
+                usernameErrorMessage: '',
+                isPwdError: false,
+                pwdErrorMessage: '',
       
             }
         },
         methods: {
             async loginFunc() {
             console.log(this.username, this.password);
+            if(!this.username && !this.password){
+                this.isUsernameError = true;
+                this.usernameErrorMessage = "Username must not be empty";
+                this.isPwdError = true;
+                this.pwdErrorMessage = "Password must not be empty";
+                return;
+            }
+            if(!this.username){
+                this.isUsernameError = true;
+                this.usernameErrorMessage = "Username must not be empty";
+                return;
+            }
+            if(!this.password){
+                this.isPwdError = true;
+                this.pwdErrorMessage = "Password must not be empty";
+                return;
+            }
             try {
                 const result = await axios.post(ApiConstant.LoginURL, {
                     username: this.username,
                     password: this.password,
                     isMobileLogin: false
                 });
-
+                console.log("hello "+result.status); 
                 if (result.status === 200) {
-                console.log('sign in done');
-                localStorage.setItem("token", JSON.stringify(result.data));
-                this.$router.push({ name: 'Home' });
-                } else {
-                console.error('Unexpected response:', result);
-                alert('Invalid Login');
+                    console.log('sign in done');
+                    localStorage.setItem("token", JSON.stringify(result.data));
+                    this.$router.push({ name: 'Home' });
                 }
             } catch (error) {
-                console.error('Error in login', error);
-                alert('An error occurred during login. Please try again.');
+                this.isUsernameError = true;
+                this.isPwdError = true;
+                this.pwdErrorMessage = "Username or password is invalid";
             }
             },
             
@@ -89,11 +112,11 @@ svg{
 img.logo{
     width: 150px;
 }
-button{
+.btn{
     color: var(--dark-color);
     background-color: var(--light-color);
 }
-button:hover{
+.btn:hover{
     border: 1px solid var(--dark-color);
     color: #FFF;
     background-color: var(--gold-neutral-color);
@@ -109,5 +132,11 @@ input[type="text"]:focus,
 input[type="password"]:focus{
     border: none;
     box-shadow: 0 0 0 0.2rem rgba(189,153,87, 0.8);
+}
+.text-danger{
+    color: rgb(163, 27, 27);
+}
+.error-sign{
+    border: 1px solid rgb(196, 31, 31);
 }
 </style>
